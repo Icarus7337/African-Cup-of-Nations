8babd59b566f4cc56d0e438a03917e326b1e6b9a
Rahul Saranjame <rsaranjame@gmail.com>
2018-07-04 22:09:44 +0530

    Add files via upload

---
+++ yearData.js
@@ -0,0 +1,83 @@
+var yearData = [
+	{
+      "year": "1978",
+      "host": "Ghana",
+	  "winner": "Ghana",
+	  "loser": "Uganda",
+		"lat": "5.5515251",
+		"long": "-0.191943",
+		"stadiumName": "Accra Sports Stadium"
+    },
+	{
+      "year": "1980",
+      "host": "Nigeria",
+	  "winner": "Nigeria",
+	  "loser": "Algeria",
+		"lat": "6.497186",
+		"long": "3.364714",
+		"stadiumName": "Lagos National Stadium"
+    },
+	{
+      "year": "1982",
+      "host": "Libya",
+	  "winner": "Ghana",
+	  "loser": "Libya",
+		"lat": "32.8602789",
+		"long": "13.1354711",
+		"stadiumName": "Tripoli Stadium"
+    },
+	{
+      "year": "1984",
+      "host": "Ivory Coast",
+	  "winner": "Cameroon",
+	  "loser": "Nigeria",
+		"lat": "5.328240",
+		"long": "-4.018785",
+		"stadiumName": "Stade Felix Houphouet-Boigny"
+    },
+	{
+      "year": "1986",
+      "host": "Egypt",
+	  "winner": "Egypt",
+	  "loser": "Cameroon",
+		"lat": "30.0691131",
+		"long": "31.3122579",
+		"stadiumName": "Cairo International Stadium"
+    },
+	{
+      "year": "1988",
+      "host": "Morocco",
+	  "winner": "Cameroon",
+	  "loser": "Nigeria",
+		"lat": "33.5827953",
+		"long": "-7.6467228",
+		"stadiumName": "Stade Mohammed V"
+    },
+	{
+      "year": "1990",
+      "host": "Algeria",
+	  "winner": "Algeria",
+	  "loser": "Nigeria",
+		"lat": "36.7599253",
+		"long": "2.99546510",
+		"stadiumName": "El Djezair Stadium"
+    },
+	{
+      "year": "1992",
+      "host": "Senegal",
+	  "winner": "Ivory Coast",
+	  "loser": "Ghana",
+		"lat": "14.7464194",
+		"long": "-17.4525715",
+		"stadiumName": "Leopold Sedar Senghor Stadium"
+    },
+	{
+	  "year": "1994",
+      "host": "Tunisia",
+	  "winner": "Nigeria",
+	  "loser": "Zambia",
+		"lat": "36.839793",
+		"long": "10.185384",
+		"stadiumName": "Stade Olympique El Menzah"
+	}
+]
